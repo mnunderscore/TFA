b7baf410641182ab5b3982301dfa1f7a1cecdded
mnunderscore <33391370+mnunderscore@users.noreply.github.com>
2024-05-23 15:25:21 +0200

    so many changes
    - title, lists, canvas, content!
    - moving everything into a single page
    - implementing anchors and color change in jQuery
    - other stuff idk

--- src/scripts/trackbuilder.js
+++ src/scripts/trackbuilder.js
@@ -1,35 +1,6 @@
 'use strict'
 
 console.clear();
-import $ from 'jquery';
-
-function changeColorIfVisible() {
-    const leftColumn = $('.left-column');
-    const topOfViewport = leftColumn.scrollTop();
-    const bottomOfViewport = topOfViewport + leftColumn.height();
-
-    $('.content__title').each(function() {
-        const element = $(this);
-        const topOfElement = element.offset().top - leftColumn.offset().top + topOfViewport;
-        const bottomOfElement = topOfElement + element.outerHeight();
-
-        if (topOfElement >= topOfViewport && bottomOfElement <= bottomOfViewport) {
-            // The entire element is visible, change its color to green
-            element.css('color', '#95FF00');
-            element.css('font-weight', '700')
-        } else {
-            // The entire element is not visible, change its color back to original
-            element.css('color', '');
-            element.css('font-weight', '')
-        }
-    });
-}
-
-// Call the function initially
-changeColorIfVisible();
-
-// Call the function whenever the user scrolls in .left-column
-$('.left-column').scroll(changeColorIfVisible);
 
 const rand = ( max = 1, min = 0, { round = false } = {} ) => {
     let n = Math.random() * ( max - min ) + min;
