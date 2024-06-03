201243cd615c2fcddc5291a821d70ef76a5c7c0e
mnunderscore <33391370+mnunderscore@users.noreply.github.com>
2024-06-03 14:49:18 +0200

    gsap loading page anims

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,7 @@
 'use strict'
 import $ from 'jquery';
 import 'trackbuilder.js';
+import { gsap } from "gsap";
 
 function changeColorIfVisible() {
     const leftColumn = $('.left-column');
@@ -67,6 +68,73 @@ function highlightCurrentSection() {
     }
 }
 
+
+var timeline = gsap.timeline();
+
+timeline.fromTo(".animation .title",{
+    opacity:0,
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+},{
+    opacity:1,
+    duration: 1,
+    ease: "power1.out",
+},"0");
+timeline.fromTo(".animation .title",{
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    duration: 3,
+},{
+    opacity:1,
+    top: "35%",
+    duration: 1,
+    ease: "power1.out",
+    delay: 2
+},">");
+timeline.fromTo(".animation .subtitle",{
+    opacity:0,
+    visibility: "hidden",
+    height:0,
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    position: "absolute",
+    duration: 1,
+},{
+    opacity:1,
+    visibility: "visible",
+    height: "auto",
+    duration:1,
+    ease: "power1.out",
+},">");
+timeline.to(".animation .title",{
+    opacity: 0,
+    height:0,
+    duration: 0.5,
+    delay: 3,
+    ease: "power1.out",
+},">");
+timeline.to(".animation .subtitle",{
+    opacity: 0,
+    height:0,
+    duration: 0.5,
+    ease: "power1.out",
+},"<");
+timeline.to(".animation .title",{
+    visibility: "hidden",
+},">");
+timeline.to(".animation .subtitle",{
+    visibility: "hidden",
+},"<");
+timeline.from(".page__container",{
+    opacity: 0,
+    duration: 1,
+    ease: "power1.out",
+},">");
 
 changeColorIfVisible();
 highlightCurrentSection();
